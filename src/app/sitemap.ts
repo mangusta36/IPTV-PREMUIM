a1f935c18d695bd12432d06093b1a516bb69e2ac
ikosoft <fullstackwebdevlopment3030@gmail.com>
2026-06-03 14:05:43 +0100

    msg done

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from 'next';
 import { blogPosts } from '@/lib/blog-data';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://www.iflexiptv.com';
+  const baseUrl = 'https://www.iflexiptv.pro';
 
   const staticRoutes = [
     '',
